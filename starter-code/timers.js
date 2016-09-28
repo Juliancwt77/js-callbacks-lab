5e18d6abe7f30a8cc4a88a4e4c7ac12483a39abd
Julian Chung <juliancwt77@gmail.com>
2016-09-29 01:13:18 +0800

    updates

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,68 @@
-// your code here:
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+
+  var count = 0;
+
+function timerId() {
+
+    window.setInterval(startButton,1000);
+
+}
+
+
+  function startButton() {
+
+    document.getElementById("timer").textContent = " Time elapsed: " + count;
+    count++;
+
+  }
+
+
+  document.getElementById("start").addEventListener("click", function() {
+
+    startButton();
+    timerId();
+
+  });
+
+
+
+  function resetButton() {
+
+
+
+    document.getElementById("timer").textContent = "Stop Watch";
+
+    window.clearInterval(timerId);
+  }
+
+document.getElementById("reset").addEventListener("click", function() {
+
+     resetButton();
+     window.clearInterval(startButton);
+
+
+});
+
+  function pauseButton() {
+
+    ocument.getElementById("reset").addEventListener("click", function() {
+
+
+    window.clearInterval(startButton);
+
+  });
+
+}
+
+
+
+
+
+
+
+
+}
+
+init()
